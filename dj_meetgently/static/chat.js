11f5e09bc43ac944a796942c7af47931f4b8136e
a110482 <a110482@yahoo.com.tw>
2017-03-26 20:13:40 +0800

    123

--- dj_meetgently/static/chat.js
+++ dj_meetgently/static/chat.js
@@ -4,7 +4,7 @@
 $(function() {
     // When we're using HTTPS, use WSS too.
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-    var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);
+    var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/new_socket/chat" + window.location.pathname);
 
     chatsock.onmessage = function (message) {
         var data = JSON.parse(message.data);
