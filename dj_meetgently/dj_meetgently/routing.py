090ba4c36c3c76db2545345eec6391dcff91bd04
a110482 <a110482@yahoo.com.tw>
2017-03-27 18:10:39 +0800

    123

--- dj_meetgently/dj_meetgently/routing.py
+++ dj_meetgently/dj_meetgently/routing.py
@@ -11,13 +11,7 @@ from channels import route
 # }
 
 channel_routing = [
-    route(u"http.request", consumers.ws_connect, path=r'^new_socket/chat/$'),
-    route(u"websocket.connect", consumers.ws_connect),
-
-    # Called when WebSockets get sent a data frame
-    route(u"websocket.receive", consumers.ws_receive),
-
-    # Called when WebSockets disconnect
-    route(u"websocket.disconnect", consumers.ws_disconnect),
-
+    # route(u"websocket.connect", consumers.ws_connect),
+    # route(u"websocket.receive", consumers.ws_receive),
+    # route(u"websocket.disconnect", consumers.ws_disconnect),
 ]
