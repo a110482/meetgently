1653f51f7f3dcdd5a3171963caf2ba116d318a57
a110482 <a110482@yahoo.com.tw>
2017-03-27 21:59:45 +0800

    123

--- dj_meetgently/dj_meetgently/settings.py
+++ dj_meetgently/dj_meetgently/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4@6^9=xl3+i$o7e=!g804tkydjwf0gb065ynj$qaa=zv#-i_74'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ws', 'wss']
 
 
 # Application definition
