82b8c8d3fb20e6b135b00fd463511ffc951227be
a110482 <a110482@yahoo.com.tw>
2017-03-27 17:42:39 +0800

    123

--- dj_meetgently/dj_meetgently/routing.py
+++ dj_meetgently/dj_meetgently/routing.py
@@ -2,7 +2,7 @@ from channels.staticfiles import StaticFilesConsumer
 from . import consumers
 
 channel_routing = {
-    'http.request': StaticFilesConsumer(),
+    'http.request': consumers.ws_connect,
     'websocket.connect': consumers.ws_connect,
     'websocket.receive': consumers.ws_receive,
     'websocket.disconnect': consumers.ws_disconnect,
