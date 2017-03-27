f3fcecf36987494b2a491f6a42d8099df827636e
a110482 <a110482@yahoo.com.tw>
2017-03-27 17:54:15 +0800

    123

--- dj_meetgently/dj_meetgently/routing.py
+++ dj_meetgently/dj_meetgently/routing.py
@@ -1,9 +1,20 @@
 from channels.staticfiles import StaticFilesConsumer
 from . import consumers
+from channels import route
+
+# channel_routing = {
+#     'http.request': StaticFilesConsumer(),
+#     'websocket.connect': consumers.ws_connect,
+#     'websocket.receive': consumers.ws_receive,
+#     'websocket.disconnect': consumers.ws_disconnect,
+# }
 
 channel_routing = {
-    'http.request': StaticFilesConsumer(),
-    'websocket.connect': consumers.ws_connect,
-    'websocket.receive': consumers.ws_receive,
-    'websocket.disconnect': consumers.ws_disconnect,
+    route("websocket.connect", consumers.ws_connect, path=r'^new_socket/chat/$'),
+
+    # Called when WebSockets get sent a data frame
+    route("websocket.receive", consumers.ws_receive),
+
+    # Called when WebSockets disconnect
+    route("websocket.disconnect", consumers.ws_disconnect),
 }
